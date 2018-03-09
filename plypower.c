9fb356a31bbfb0130f7b0c96a9702921c21ea2ff
manobharathivsb <31505612+manobharathivsb@users.noreply.github.com>
2018-03-09 15:51:30 +0530

    Create plypower.c

---
+++ plypower.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a,i;
+	scanf("%d",&a);
+            for(i=1;i<=a;i++)	 
+            if(pow(2,i)==a) 
+            printf("yes");
+	return 0;
+}
