04c5a20bc10ea0a7b0b3ef639c2e18ebb7bdb75c
manobharathivsb <31505612+manobharathivsb@users.noreply.github.com>
2018-03-15 11:21:30 +0000

    Update gcd.c

--- gcd.c
+++ gcd.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 int main(void)
 {
-	int a,b,i,d;
-	scanf("%d%d",&a,&b);
+	int a,b,i,c;
+	scanf("%d\t%d",&a,&b);
 	{
-		for(i=0;i<=10;i++)
-		{
+		for(i=1;i<=10;i++)
 		if(a%i==0 && b%i==0)
-		{
-			d=i;
-		}
-		printf("%d",d);
+			c=i;
+		printf("%d",c);
 		}
 		return 0;
-	}
 }
